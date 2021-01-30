16e2c69d8b73ab16fd458fd6e3cab725c058baf9
BİLAL KALKAN <20184029007@std.nisantasi.edu.tr>
2021-01-31 00:11:13 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DateAccess.Abstract;
+using DateAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal _ProductDal)
+        {
+            _productDal = _ProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //yetkisi var mı?
+            return _productDal.GetAll();
+        }
+    }
+}
